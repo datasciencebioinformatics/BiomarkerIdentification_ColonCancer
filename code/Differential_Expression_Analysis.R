c49c43d5dd5a08bb84aca757187767992125bab4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-29 06:47:41 -0300

    Add files via upload

---
+++ code/Differential_Expression_Analysis.R
@@ -0,0 +1,19 @@
+########################################################################
+# First phase, Tissue.Type
+# Create DESeqDataSet from your prepared matrix
+dds_tumor_normal <- DESeqDataSetFromMatrix(countData = read_counts_table,
+                              colData = sample_sheet_data,
+                              design = ~ Tissue.Type)
+# Run DeSeq2
+dds_tumor_normal <- DESeq(dds_tumor_normal)
+
+# Obtain the results
+res_tumor_normal <- results(dds_tumor_normal, contrast=c("Tissue.Type","Tumor","Normal"))
+
+# Take 
+res_tumor_normal<-data.frame(res_tumor_normal[which(res_tumor_normal$padj<0.05 & abs(res_tumor_normal$log2FoldChange)>2),])
+########################################################################
+# 2. Save the object to an .rds file
+saveRDS(dds_tumor_normal, file = file.path(project_folder, "/rsd","/dds_tumor_normal.rsd" ))
+saveRDS(res_tumor_normal, file = file.path(project_folder, "/rsd","/res_tumor_normal.rsd" ))
+
---
+++ code/Generate_read_counts_table.R
@@ -0,0 +1,86 @@
+###################################################################################
+# Set the project configuration
+# Set the path to Sample sheet filtered file
+Sample_sheet_filtered_file=paste(project_folder,"metadata/gdc_sample_sheet_star_gene_counts.tsv",sep="")
+
+# Set the path to clinical data file
+clinical_data_file=paste(project_folder,"metadata/clinical.tsv",sep="")
+
+
+# Take the list of gene ids
+gene_ids_file<-paste(project_folder,"metadata/gene_ids.txt",sep="")
+###################################################################################
+# Load sample sheet data
+sample_sheet_data<-read.delim(file = Sample_sheet_filtered_file, sep = '\t', header = TRUE,fill=TRUE)
+
+# Load gene ids data
+gene_ids_data<-read.delim(file = gene_ids_file, sep = '\t', header = TRUE,fill=TRUE)
+###################################################################################
+# Star dataframe with all gene ids unstranded
+read_counts_table<-data.frame(gene_id=as.vector(gene_ids_data[,1]))
+
+# Star dataframe with all gene ids tpm_unstranded
+read_counts_table_tpm<-data.frame(gene_id=as.vector(gene_ids_data[,1]))
+
+# Set rownames unstranded
+rownames(read_counts_table)<-read_counts_table$gene_id
+
+# Set rownames tpm_unstranded
+rownames(read_counts_table_tpm)<-read_counts_table_tpm$gene_id
+
+# Set rownames of 
+rownames(sample_sheet_data)<-sample_sheet_data$File.ID
+
+# For each file in the sample_sheet_data, read the gene counts data
+for (sample in rownames(sample_sheet_data))
+{
+  # Take the file id
+  file_id<-sample_sheet_data[sample,"File.ID"]
+
+  # Take the name of the file
+  file_name<-sample_sheet_data[sample,"File.Name"]
+
+  # Read the counts table
+  counts_table<-read.delim(paste(project_folder,"data/",file_name,sep=""), skip = 5)
+
+  # Set the colnames
+  colnames(counts_table)<-c("gene_id",	"gene_name",	"gene_type",	"unstranded",	"stranded_first",	"stranded_second",	"tpm_unstranded",	"fpkm_unstranded",	"fpkm_uq_unstranded")
+
+  # Set rownames
+  rownames(counts_table)<-counts_table$gene_id
+
+  # Start data.frame for the table unstranded
+  df_counts_table_unstranded<-data.frame(file_id=counts_table[rownames(counts_table),"unstranded"])
+
+  # Start data.frame for the table unstranded
+  df_counts_table_tpm<-data.frame(file_id=counts_table[rownames(counts_table),"tpm_unstranded"])  
+
+  # Set file_id in the colname
+  colnames(df_counts_table_unstranded)<-file_id
+  colnames(df_counts_table_tpm)<-file_id
+
+  # Add to table unstranded
+  read_counts_table<-cbind(read_counts_table,df_counts_table_unstranded)
+
+  # Add to table tpm_unstranded
+  read_counts_table_tpm<-cbind(read_counts_table_tpm,df_counts_table_tpm)  
+}
+# Set rownames unstranded
+rownames(read_counts_table)<-read_counts_table$gene_id
+
+# Set rownames unstranded
+rownames(read_counts_table_tpm)<-read_counts_table_tpm$gene_id
+
+# Remove first collumn unstranded
+read_counts_table<-read_counts_table[,-1]
+
+# Remove first collumn tpm_unstranded
+read_counts_table_tpm<-read_counts_table_tpm[,-1]
+
+# 2. Save the object to an .rds file
+saveRDS(df_counts_table_unstranded, file = file.path(project_folder, "/rsd","/df_counts_table_unstranded.rsd" ))
+saveRDS(df_counts_table_tpm, file = file.path(project_folder, "/rsd","/df_counts_table_tpm.rsd" ))
+saveRDS(sample_sheet_data, file = file.path(project_folder, "/rsd","/sample_sheet_data.rsd" ))
+
+
+
