eaa1d2f424c603ffad47858991fd108890f1188d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-01 14:11:44 -0500

    Update Assess_metadata.R

--- code/Assess_metadata.R
+++ code/Assess_metadata.R
@@ -28,7 +28,34 @@ dev.off()
 # Add the count table
 df2<-reshape2::melt(table(metadata[,c("demographic.gender", "samples.tissue_type")]),by="demographic.gender")
 
-p1<-ggplot(metadata, aes(y = samples.tissue_type)) +  geom_bar(aes(fill = demographic.gender), position = position_stack(reverse = TRUE)) +  theme(legend.position = "top") + theme_bw()+  theme(legend.position = "bottom",panel.grid = element_blank())  + geom_text( stat = 'count', aes(label = after_stat(count)), position = position_dodge(width = 0.9), vjust = -0.5 )
+# Sort by dose and supp
+df_sorted <- df2
+
+# Calculate the cumulative sum of len for each dose
+df_cumsum <- ddply(df_sorted, "value", transform, label_ypos=cumsum(value))
+
+# Add the total male
+df_cumsum$total_male <-sum(df_cumsum[which(df_cumsum$demographic.gender=="male"),"value"])
+df_cumsum$total_female <-sum(df_cumsum[which(df_cumsum$demographic.gender=="female"),"value"])
+
+# Take the total male and total female
+df_cumsum$total_male_tumor      <-sum(df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Tumor" ),"value"])
+df_cumsum$total_female_tumor    <-sum(df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Tumor" ),"value"])
+
+# Set the y-label for tumor samples
+df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Tumor" ),"label_ypos"] <- df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Tumor" ),"total_male_tumor"] /2
+df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Tumor" ),"label_ypos"] <- df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Tumor" ),"total_female_tumor"] /2
+
+# Set the y-label for normal samples
+df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Normal" ),"label_ypos"] <-df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Tumor" ),"total_male_tumor"]+(df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Tumor" ),"total_male"]-df_cumsum[which(df_cumsum$demographic.gender=="male" & df_cumsum$samples.tissue_type=="Tumor" ),"total_male_tumor"])/2
+df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Normal" ),"label_ypos"] <-df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Tumor" ),"total_female_tumor"]+(df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Tumor" ),"total_female"]-df_cumsum[which(df_cumsum$demographic.gender=="female" & df_cumsum$samples.tissue_type=="Tumor" ),"total_female_tumor"])/2
+
+# Create the barplot
+p1<-ggplot(data=df_cumsum, aes(x=demographic.gender, y=value, fill=samples.tissue_type)) +  geom_bar(stat="identity")+  geom_text(aes(y=label_ypos, label=value), vjust=1.6,  color="white", size=3.5)+  scale_fill_brewer(palette="Paired")+  theme_minimal()  + theme(legend.position = "bottom",panel.grid = element_blank())  + coord_flip()
+
+
+
+
  
 "demographic.gender", "samples.tissue_type"
 "demographic.race" with "samples.tissue_type"
