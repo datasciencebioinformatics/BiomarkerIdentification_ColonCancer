18ec255a7d813fbc9b723e997371e7558c5b4615
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-14 06:11:34 -0500

    Create Biomarker_selection.R

---
+++ code/Biomarker_selection.R
@@ -0,0 +1,9 @@
+# Define resampling method (e.g., 10-fold cross-validation)
+train_control <- trainControl(method = "cv", number = 10)
+
+# Train the model using stepwise AIC
+step_model <- train(Tissue.Type ~ ., 
+                    data = read_counts_table, 
+                    method = "lmStepAIC", 
+                    trControl = train_control,
+                    trace = FALSE) # Set trace = FALSE to suppress iteration output
