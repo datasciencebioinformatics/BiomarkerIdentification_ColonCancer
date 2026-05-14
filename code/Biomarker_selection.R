d10cfd320779aa67867a8a6d6070dc4a9ee02fbf
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-14 06:20:15 -0500

    Update Biomarker_selection.R

--- code/Biomarker_selection.R
+++ code/Biomarker_selection.R
@@ -2,8 +2,22 @@
 train_control <- trainControl(method = "cv", number = 10)
 
 # Train the model using stepwise AIC
-step_model <- train(Tissue.Type ~ ., 
+step_model_lm <- train(Tissue.Type ~ ., 
                     data = read_counts_table, 
                     method = "lmStepAIC", 
+                    trControl = train_control,
+                    trace = FALSE) # Set trace = FALSE to suppress iteration output
+
+# Train the model using stepwise AIC
+decision_tre <- train(Tissue.Type ~ ., 
+                    data = read_counts_table, 
+                    method = "rpart", 
+                    trControl = train_control,
+                    trace = FALSE) # Set trace = FALSE to suppress iteration output
+
+# Train the model using stepwise AIC
+decision_tre <- train(Tissue.Type ~ ., 
+                    data = read_counts_table, 
+                    method = "rf", 
                     trControl = train_control,
                     trace = FALSE) # Set trace = FALSE to suppress iteration output
