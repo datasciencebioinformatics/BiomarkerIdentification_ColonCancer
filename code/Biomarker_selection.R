9af3c661481a62099c47f5b7119d54819d011139
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-14 07:01:50 -0500

    Update Biomarker_selection.R

--- code/Biomarker_selection.R
+++ code/Biomarker_selection.R
@@ -9,15 +9,32 @@ step_model_lm <- train(Tissue.Type ~ .,
                     trace = FALSE) # Set trace = FALSE to suppress iteration output
 
 # Train the model using stepwise AIC
-decision_tre <- train(Tissue.Type ~ ., 
+decision_tree <- train(Tissue.Type ~ ., 
                     data = read_counts_table, 
                     method = "rpart", 
                     trControl = train_control,
                     trace = FALSE) # Set trace = FALSE to suppress iteration output
 
+
+
+# bwplot               
+png(filename=paste(output_dir,"Tissue_Type_rpart.png",sep=""), width = 15, height = 15, res=600, units = "cm")  
+  # Plot the bayesian network graph
+  fancyRpartPlot(Head_rpart, caption = NULL, sub=NULL)  
+dev.off()
+
 # Train the model using stepwise AIC
-decision_tre <- train(Tissue.Type ~ ., 
+random_forest <- train(Tissue.Type ~ ., 
                     data = read_counts_table, 
                     method = "rf", 
                     trControl = train_control,
                     trace = FALSE) # Set trace = FALSE to suppress iteration output
+
+# Get Variable Importance
+importance <- varImp(random_forest)
+
+# bwplot               
+png(filename=paste(output_dir,"Tissue_Type_varIMP.png",sep=""), width = 15, height = 15, res=600, units = "cm")  
+  # Plot Variable Importance
+  plot(importance, main="")
+dev.off()
