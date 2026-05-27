809ae64a354e876316b25cf9a28e1d24f065ddde
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-27 09:45:03 -0500

    Update Biomarker_selection.R

--- code/Biomarker_selection.R
+++ code/Biomarker_selection.R
@@ -36,30 +36,5 @@ df_importance <- df_importance[order(-df_importance$Overall), ]
 df_importance_top <-  head(df_importance,n=10)
 
 # Save the top selected biomarkers
-write_xlsx(df_mean[rownames(df_importance_top),], paste(output_dir,"df_importance_top.xlsx",sep=""))
+write_xlsx(df_importance_top[rownames(df_importance_top),], paste(output_dir,"df_importance_top.xlsx",sep=""))
 
-###############################################################
-# 1. Regressão linear múltipla (com seleção stepwise por AIC)
-# A seleção de modelo deve ser realizada utilizando o método stepwise regression com abordagem backward elimination, tendo como critério único a minimização do Akaike Information Criterion (AIC) para cada experimento e para cada variável de saída. A seleção deve ser realizada exclusivamente com dados de train. Nenhuma informação de test pode ser utilizada na seleção.
-# For the multiple linear regression, lm method was used to fit the trainning set on the surrogate models. StepAIC function was called to select the best model based on AIC. The trainning set was again used to fit on the selected model and obtain the prediction statistitics (RMSE, Rsquared, MAE with SD).
-
-# lm method was used to fit the trainning set on the surrogate models
-multiple_linear_regression_lm <- lm(Tissue_Type ~ ., data = df_read_counts_table)
-
-# Perform stepwise selection (direction "both", "backward", "forward")
-surrogate_model_final_lm <- stepAIC(multiple_linear_regression_lm, direction = "backward", trace = 0)
-
-# Train the linear model
-multiple_linear_regression_lm_selected <- caret::train(formula(surrogate_model_final_lm), data = training_set, method = "lm", trControl = train_control)
-
-#########################################################################################################
-df_read_counts_table$Tissue_Type<-as.numeric(df_read_counts_table$Tissue_Type)
-
-# Create bayesian networks
-bn_viscour <- hc(df_read_counts_table)
-
-# bwplot               
-png(filename=paste(output_dir,"Bayesian_Network_structure.png",sep=""), width = 17, height = 17, res=600, units = "cm")  
-  # Plot the bayesian network graph
-  plot(as.igraph(tb_viscour), vertex.color="black",vertex.size=25,vertex.label.color="orange",layout=layout_with_kk)
-dev.off()
