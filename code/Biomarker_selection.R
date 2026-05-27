f39bb7b84890b2773a275610eed3b88b3795fb46
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-27 09:54:09 -0500

    Update Biomarker_selection.R

--- code/Biomarker_selection.R
+++ code/Biomarker_selection.R
@@ -2,23 +2,23 @@
 train_control <- trainControl(method = "repeatedcv", number = 10)
 
 # Add Tissue_Type collunn sample_sheet_data
-df_read_counts_table<-data.frame(t(read_counts_table[rownames(res_tumor_normal),]),Tissue_Type=sample_sheet_data[colnames(read_counts_table),"Tissue.Type"])
+df_counts_table_tpm_cp<-data.frame(t(df_counts_table_tpm[rownames(res_tumor_normal),]),Tissue_Type=sample_sheet_data[colnames(df_counts_table_tpm),"Tissue.Type"])
 
 # Cobvert to factor
-df_read_counts_table$Tissue_Type<-factor(df_read_counts_table$Tissue_Type)
+df_counts_table_tpm_cp$Tissue_Type<-factor(df_counts_table_tpm_cp$Tissue_Type)
 
 # decision tree
-decision_tree <-rpart(Tissue_Type ~ ., data = df_read_counts_table, method = "class")
+decision_tree <-rpart(Tissue_Type ~ ., data = df_counts_table_tpm_cp, method = "class")
 
 # bwplot               
 png(filename=paste(output_dir,"Tissue_Type_rpart.png",sep=""), width = 10, height = 10, res=600, units = "cm")  
-  # Plot the bayesian network graph
+  # Plot the decision tree
   fancyRpartPlot(decision_tree, caption = NULL, sub=NULL)  
 dev.off()
 
 # Train the model using stepwise AIC
 random_forest <- train(Tissue_Type ~ ., 
-                    data = df_read_counts_table, 
+                    data = df_counts_table_tpm_cp, 
                     method = "rf", 
                     trControl = train_control,
                     trace = FALSE) # Set trace = FALSE to suppress iteration output
