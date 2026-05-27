857fa85f87ba0e4119cd97702c36951e2d0ac8db
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-05-27 11:19:33 -0500

    Update Biomarker_selection.R

--- code/Biomarker_selection.R
+++ code/Biomarker_selection.R
@@ -1,5 +1,25 @@
-# Define resampling method (e.g., 10-fold cross-validation)
-train_control <- trainControl(method = "repeatedcv", number = 10)
+# 1. Configurar o método de reamostragem (Validação Cruzada)
+# define trainControl
+# Define train control parameters (e.g., 10-fold cross-validation)
+# Custom summary function for MRE 
+# Custom summary function for MRE
+mreSummary <- function(data, lev = NULL, model = NULL) {
+	rmse=rmse(data$obs, data$pred) 
+	mae=mae(data$obs, data$pred)
+	mre=mre(data$obs, data$pred) 
+	cor=cor(data$obs, data$pred)
+
+	# If NA, correct
+	if (is.null(rmse)) {rmse=NA}
+	if (is.null(mae)) {mae=NA}
+	if (is.null(mre)) {mre=NA}
+	
+  c(MRE = mre, RMSE=rmse, MAE = mae, Cor=cor)
+}
+
+# Set the trainning function
+train_control <- trainControl(method = "cv", number = 10, verboseIter = TRUE, summaryFunction = mreSummary)
+########################################################################################################################################################################
 
 # Add Tissue_Type collunn sample_sheet_data
 df_counts_table_tpm_cp<-data.frame(t(read_counts_table_tpm[rownames(res_tumor_normal),]),Tissue_Type=sample_sheet_data[colnames(read_counts_table_tpm),"Tissue.Type"])
@@ -15,6 +35,16 @@ png(filename=paste(output_dir,"Tissue_Type_rpart.png",sep=""), width = 10, heigh
   # Plot the decision tree
   fancyRpartPlot(decision_tree, caption = NULL, sub=NULL)  
 dev.off()
+
+# 6. Model for combination of parameter
+model_comb <- caret::train(Tissue_Type ~ ., data = df_counts_table_tpm_cp, method = "rpart", trControl = train_control)
+
+
+rmse=round(model_comb$results$RMSE,2)
+mae=round(model_comb$results$MAE,2)
+mre=round(model_comb$results$MRE,2) 
+cor=round(model_comb$results$Cor,2)
+################################################################################################################################################################################3
 
 # Train the model using stepwise AIC
 random_forest <- train(Tissue_Type ~ ., 
@@ -37,4 +67,8 @@ df_importance_top <-  head(df_importance,n=10)
 
 # Save the top selected biomarkers
 write_xlsx(df_importance_top[rownames(df_importance_top),], paste(output_dir,"df_importance_top.xlsx",sep=""))
+
+# 6. Model for combination of parameter
+model_comb <- caret::train(Tissue_Type ~ ., data = df_counts_table_tpm_cp, method = "rpart", trControl = train_control)
+
 
